e3ba62946cc9f6177b1ac2cb4effe9fc4dbecba3
D0berm4n <wotunion@gmail.com>
2017-08-11 11:56:41 +0300

    maintenance

--- zip_dist.cmd
+++ zip_dist.cmd
@@ -1,1 +1,1 @@
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('dist', 'POEWeaponDPSCalculator_0.9.1.zip'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('dist', 'POELevelingGuide_0.9.1.zip'); }"
